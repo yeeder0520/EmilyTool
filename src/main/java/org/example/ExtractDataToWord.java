c453ac479ab673ec383a3c51bc27bd66df37192c
yeeder0520 <59109960+yeeder0520@users.noreply.github.com>
2024-05-05 02:00:56 +0800

    init

--- src/main/java/org/example/ExtractDataToWord.java
+++ src/main/java/org/example/ExtractDataToWord.java
@@ -5,6 +5,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Pattern;
 import org.apache.pdfbox.io.RandomAccessFile;
 import org.apache.pdfbox.pdfparser.PDFParser;
@@ -16,27 +20,66 @@ import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 
 public class ExtractDataToWord {
 
+  Map<String, List<String>> carNumberData = new HashMap<>();
+
   public void execute(File[] pdfFiles) throws IOException {
     // Initialize the resultDoc outside the loop
+    initCarNumberMap();
+
     try (XWPFDocument resultDoc = new XWPFDocument()) {
 
       for (File pdfFile : pdfFiles) {
-        System.out.println("Processing file: " + pdfFile.getAbsolutePath());
-        String result = getPdfContent(pdfFile.getAbsolutePath());
-        String[] parts = result.split("\\s+");
-
-        XWPFTable table = resultDoc.createTable();
-        setupTable(table);
-
-        XWPFTableRow row = table.createRow();
-        fillTable(parts, row, table);
-        resultDoc.createParagraph()
-            .createRun()
-            .addBreak();
+        getPdfContent(pdfFile.getAbsolutePath());
       }
+
+      carNumberData.forEach((key, value) -> {
+        XWPFTable table = null;
+        try {
+
+          if (!value.isEmpty()) {
+            // 在第一行印出車牌號碼
+            resultDoc.createParagraph()
+                .createRun()
+                .setText("車牌號碼: " + key);
+            table = resultDoc.createTable();
+            setupTable(table);
+          }
+          for (String content : value) {
+
+            XWPFTableRow row = table.createRow();
+
+            String[] parts = content.split("\\s+");
+
+            fillTable(parts, row, table);
+            resultDoc.createParagraph()
+                .createRun()
+                .addBreak();
+          }
+
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      });
+
+//      String[] parts = result.split("\\s+");
+//        XWPFTable table = resultDoc.createTable();
+//        setupTable(table);
+//
+//        XWPFTableRow row = table.createRow();
+//        fillTable(parts, row, table);
+//        resultDoc.createParagraph()
+//            .createRun()
+//            .addBreak();
 
       saveDocument(resultDoc, pdfFiles[0].getParent());
     }
+  }
+
+  private void initCarNumberMap() {
+    carNumberData.put("AMD-3275", new ArrayList<>());
+    carNumberData.put("AHT-9309", new ArrayList<>());
+    carNumberData.put("BDC-7297", new ArrayList<>());
+    carNumberData.put("0185-NB", new ArrayList<>());
   }
 
   private void setupTable(XWPFTable table) {
@@ -115,17 +158,28 @@ public class ExtractDataToWord {
     File file = new File(pdfFilePath);
     PDFParser parser = new PDFParser(new RandomAccessFile(file, "rw"));
     parser.parse();
+
+    String carNumber;
+
     try (PDDocument pdfDocument = parser.getPDDocument()) {
       PDFTextStripper stripper = new PDFTextStripper();
       result = stripper.getText(pdfDocument);
-      result = result.substring(result.indexOf("試算餘") + 10,
-                                result.length() - 1);
-      result = result.replace("\n", "")
-          .replace("\r", "");
-      result = result.replaceAll("(.*?)(\\d{4}/\\d{2}/\\d{2})", "$1\n$2 ");
-      result = result.replaceAll("元", "元 ");
-      result = result.replaceAll("\\s+(\\d+折優惠)", "$1");
-      result = result.replaceAll("\\s+(試算餘額低於)", "$1");
+
+      //取得車牌號碼
+      carNumber = result.substring(result.indexOf("自負一切法律責任") + 11, result.indexOf(" 餘額紀錄列印"));
+
+      if (carNumberData.containsKey(carNumber)) { //取得車牌號碼
+        result = result.substring(result.indexOf("試算餘") + 10,
+                                  result.length() - 1);
+        result = result.replace("\n", "")
+            .replace("\r", "");
+        result = result.replaceAll("(.*?)(\\d{4}/\\d{2}/\\d{2})", "$1\n$2 ");
+        result = result.replaceAll("元", "元 ");
+        result = result.replaceAll("\\s+(\\d+折優惠)", "$1");
+        result = result.replaceAll("\\s+(試算餘額低於)", "$1");
+        carNumberData.get(carNumber)
+            .add(result);
+      }
       return result;
     }
   }
